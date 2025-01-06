736c44243bbed5461c579af5da1aac3d40632a8c
Hrishikesh Dahale <55376806+HD-Codes@users.noreply.github.com>
2025-01-05 18:27:30 -0700

    comment unnecessary stuf

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { SectionHeader } from "@/components/SectionHeader";
 import { Card } from "@/components/Card";
-import bookImage from '@/assets/images/book-cover.png';
+// import bookImage from '@/assets/images/book-cover.png';
 import Image from "next/image";
 import JavascriptIcon from '@/assets/icons/square-js.svg';
 import HTMLIcon from '@/assets/icons/html5.svg';
@@ -13,7 +13,7 @@ import mapImage from "@/assets/images/map.png";
 import smileMemoji from '@/assets/images/memoji-smile.png';
 import { CardHeader } from "@/components/CardHeader";
 import { ToolboxItems } from "@/components/ToolboxItems";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import { useRef } from "react";
 
 
@@ -44,50 +44,50 @@ const toolboxItems = [
   }
 ];
 
-const hobbies = [
-  {
-    title: 'Photography',
-    emoji: '📸',
-    left: '50%',
-    top: '5%',
-  },
-  {
-    title: 'Painting',
-    emoji: '🎨',
-    left: '5%',
-    top: '5%',
-  },
-  {
-    title: 'Hiking',
-    emoji: '🥾',
-    left: '35%',
-    top: '40%',
-  },
-  {
-    title: 'Gaming',
-    emoji: '🎮',
-    left: '10%',
-    top: '35%',
-  },
-  {
-    title: 'Music',
-    emoji: '🎵',
-    left: '70%',
-    top: '45%',
-  },
-  {
-    title: 'Fitness',
-    emoji: '🏋️',
-    left: '5%',
-    top: '65%',
-  },
-  {
-    title: 'Reading',
-    emoji: '📚',
-    left: '45%',
-    top: '70%',
-  }
-];
+// const hobbies = [
+//   {
+//     title: 'Photography',
+//     emoji: '📸',
+//     left: '50%',
+//     top: '5%',
+//   },
+//   {
+//     title: 'Painting',
+//     emoji: '🎨',
+//     left: '5%',
+//     top: '5%',
+//   },
+//   {
+//     title: 'Hiking',
+//     emoji: '🥾',
+//     left: '35%',
+//     top: '40%',
+//   },
+//   {
+//     title: 'Gaming',
+//     emoji: '🎮',
+//     left: '10%',
+//     top: '35%',
+//   },
+//   {
+//     title: 'Music',
+//     emoji: '🎵',
+//     left: '70%',
+//     top: '45%',
+//   },
+//   {
+//     title: 'Fitness',
+//     emoji: '🏋️',
+//     left: '5%',
+//     top: '65%',
+//   },
+//   {
+//     title: 'Reading',
+//     emoji: '📚',
+//     left: '45%',
+//     top: '70%',
+//   }
+// ];
 
 export const AboutSection = () => {
   const constraintRef = useRef(null);
@@ -101,17 +101,20 @@ export const AboutSection = () => {
         />
         <div className="mt-20 flex flex-col gap-8">
           <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
-            <Card className="h-[320px] md:col-span-2 lg:col-span-1">
-              <CardHeader 
-                title="My Reads"
-                description="Explore the books shaping my perspectives."
-              />
-              <div className="w-40 mx-auto mt-2 md:mt-0">
+            <Card className="h-[320px] p-0 relative md:col-span-2 lg:col-span-1">
                 <Image 
-                  src={bookImage}
-                  alt="Book cover" 
+                  src={mapImage} 
+                  alt="map" 
+                  className="h-full w-full object-cover object-left-top" 
                 />
-              </div>
+                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+                  <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-20 animate-ping [animation-duration:2s]"></div>
+                  <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-10"></div>
+                  <Image 
+                    src={smileMemoji} 
+                    alt="smiling memoji" 
+                    className="size-20"/>
+                </div>
             </Card>
             <Card className="h-[320px] md:col-span-3 lg:col-span-2">
             <CardHeader 
@@ -126,7 +129,7 @@ export const AboutSection = () => {
                 itemsWrapperClassName="animate-move-right [animation-duration:20s]"/>
             </Card>
           </div>
-          <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
+          {/* <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
             <Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
             <CardHeader 
                 title="Beyond the Code"
@@ -166,7 +169,7 @@ export const AboutSection = () => {
                     className="size-20"/>
                 </div>
             </Card>
-          </div>
+          </div> */}
         </div>
       </div>
   </section>

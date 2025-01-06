1fc4d2efaa69fffea2fc4ed964208503f68c907f
Hrishikesh Dahale <55376806+HD-Codes@users.noreply.github.com>
2025-01-05 18:22:46 -0700

    add logo to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Calistoga} from "next/font/google";
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
+import logo from '@/assets/images/myLogo.png';
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 const calistoga = Calistoga({ 
@@ -11,8 +12,8 @@ const calistoga = Calistoga({
 });
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  title: "HD-CODES",
+  description: "A showcase of my professional journey, technical skills, and projects in software engineering.",
 };
 
 export default function RootLayout({
@@ -22,6 +23,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Favicon */}
+        <link rel="icon" href={logo.src} type="image/png" />
+        <title>My Portfolio</title>
+        <meta name="description" content="A portfolio website showcasing my projects and skills." />
+      </head>
       <body className={twMerge(
         inter.variable, 
         calistoga.variable, 
